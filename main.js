78fd170f1e1df73d216366b4282b1e1cdd4c64df
ThinhNeHihihi <nguyenthinhgt2@gmail.com>
2023-02-19 09:29:54 +0700

    init project

---
+++ main.js
@@ -0,0 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { render, router } from "./lib";
+import AboutPage from "./pages/about";
+import AdminAddProjectPage from "./pages/admin/projects-add";
+import AdminProjectsPage from "./pages/admin/projects";
+import ContactPage from "./pages/contact";
+import DetailProjectPage from "./pages/detail-project";
+import Homepage from "./pages/home";
+import NotFoundPage from "./pages/not-found";
+import PostsPage from "./pages/posts";
+import ProjectsPage from "./pages/projects";
+import AdminEditProjectPage from "./pages/admin/projects-edit";
+
+const app = document.querySelector("#app");
+
+router.on("/", () => render(Homepage, app));
+router.on("/about", () => render(AboutPage, app));
+router.on("/contact", () => render(ContactPage, app));
+router.on("/projects", () => render(ProjectsPage, app));
+router.on("/project/:id", ({ data }) => render(() => DetailProjectPage(data), app));
+router.on("/posts", () => render(PostsPage, app));
+
+// Admin page
+router.on("/admin/projects", () => render(AdminProjectsPage, app));
+router.on("/admin/projects/add", () => render(AdminAddProjectPage, app));
+router.on("/admin/projects/:id/edit", ({ data }) => render(() => AdminEditProjectPage(data), app));
+router.notFound(() => render(NotFoundPage, app));
+
+router.resolve();
