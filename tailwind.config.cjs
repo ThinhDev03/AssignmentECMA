e72ff161e467258dcc245c9519e38b8ca47e1117
ThinhNeHihihi <nguyenthinhgt2@gmail.com>
2023-02-19 10:06:51 +0700

    sử dụng css

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./componments/*.{html,js}", "./pages/*.{html,js}"],
+  prefix: "tw-",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
